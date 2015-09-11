226097f247e3ab4dd4ae666fcfc9a79817096775
Noel Jarencio <njarencio@gmail.com>
2015-09-11 18:35:02 +0800

    Refactor

--- game.rb
+++ game.rb
@@ -1,3 +1,10 @@
+#!/usr/bin/ruby
+
+require 'io/console'
+
+# Game settings
+WINDOW_WIDTH = 80
+
 SUITS = ['♥', '♣', '♦', '♠']
 CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
@@ -12,6 +19,46 @@ def show_cards(cards)
   cards.map { |card| card.join(' ') }
 end
 
+def puts_center(message)
+  puts message.center(WINDOW_WIDTH)
+end
+
+def print_center(message)
+  print "|"
+  print message.center(WINDOW_WIDTH)
+  print "|"
+  print "\n"
+end
+
+def show_top_border
+  (WINDOW_WIDTH + 2).times { print '-' }
+  print "\n"
+end
+
+def show_bottom_border
+  (WINDOW_WIDTH + 2).times { print '-' }
+  print "\n"
+end
+
+def show_welcome
+  system "clear"
+
+  show_top_border
+
+  print_center "Welcome to Blackjack game!"
+  print_center "Press Enter to start or Esc to quit"
+  print_center ""
+end
+
+user_input = 0
+
+# Esc key is 27 bytecode
+while user_input != 27 do
+  show_welcome
+
+  user_input = STDIN.raw(&:getbyte)
+end
+
 player = [] 
 dealer = [] 
 
@@ -23,5 +70,7 @@ deal_card_to dealer, deck
 deal_card_to player, deck
 deal_card_to dealer, deck
 
-puts "Player has: " + show_cards(player).to_s
-puts "Dealer has: " + show_cards(dealer).to_s
+print_center "Player has: " + show_cards(player).to_s
+print_center "Dealer has: " + show_cards(dealer).to_s
+
+show_bottom_border
